6b004aeaec1d143e0004bf3c04feffd0ef0ae4c1
Alex P <64188277+koihoz@users.noreply.github.com>
2020-07-03 13:25:39 +0300

    15. Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 	var form = document.querySelector(".order-form");
   var adults = document.querySelector(".order-amount-adults");
   var children = document.querySelector(".order-item-children");
+  var dates = document.querySelector(".order-item-date");
+
 
 	button.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	form.classList.toggle("visually-hidden");
+  form.classList.add("modal-show");
   localStorage.setItem("adults", adults.value);
   localStorage.setItem("children", children.value);
 	});
+
+
+  form.addEventListener("submit", function (evt) {
+   if (!aduits.value || !children.value || !dates.value) {
+     evt.preventDefault();
+   }
+ });
