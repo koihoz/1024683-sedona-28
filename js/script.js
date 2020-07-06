d466d14168a3dce96fcdb4fe39c34b3bb9f52e8b
Alex P <64188277+koihoz@users.noreply.github.com>
2020-07-06 05:36:26 +0300

    15. Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-  var button = document.querySelector(".hotel-search");
-	var form = document.querySelector(".order-form");
-  var adults = document.querySelector(".order-amount-adults");
-  var children = document.querySelector(".order-item-children");
-  var dates = document.querySelector(".order-item-date");
+var button = document.querySelector(".hotel-search");
+var form = document.querySelector(".order-form");
 
-  form.classList.add("visually-hidden");
+var adults = document.querySelector(".order-amount-adults");
+var children = document.querySelector(".order-amount-children");
+var dates = document.querySelector(".order-item-date");
 
-	button.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	form.classList.toggle("visually-hidden");
-  form.classList.toggle("modal-show");
-  localStorage.setItem("adults", adults.value);
-  localStorage.setItem("children", children.value);
-	});
 
-  form.addEventListener("submit", function (evt) {
-   if (!adults.value || !children.value || !dates.value) {
-     evt.preventDefault();
-     form.classList.add("modal-error");
-   }
- });
+form.classList.add("visually-hidden");
+
+button.addEventListener("click", function (evt) {
+evt.preventDefault();
+form.classList.toggle("visually-hidden");
+form.classList.toggle("modal-show");
+localStorage.setItem("adults", adults.value);
+localStorage.setItem("children", children.value);
+});
+
+form.addEventListener("submit", function (evt) {
+
+if (!adults.value || !children.value || !dates.value) {
+  evt.preventDefault();
+  form.classList.remove("modal-error");
+  form.offsetWidth = form.offsetWidth;
+  form.classList.add("modal-error");
+}
+
+});
